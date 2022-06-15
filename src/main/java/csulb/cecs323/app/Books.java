cb9789058075def2c93102e3cb8597f4ac16bb70
iamkellpham <Bich-Tram.Pham01@student.csulb.edu>
2022-06-14 18:24:50 -0700

    Add New Entities

--- src/main/java/csulb/cecs323/app/Books.java
+++ src/main/java/csulb/cecs323/app/Books.java
@@ -32,6 +32,7 @@ import java.util.logging.Logger;
  *     Originally provided by Dr. Alvaro Monge of CSULB, and subsequently modified by Dave Brown.
  * </p>
  */
+<<<<<<< Updated upstream:src/main/java/csulb/cecs323/app/Books.java
 public class Books {
    /**
     * You will likely need the entityManager in a great many functions throughout your application.
@@ -63,6 +64,155 @@ public class Books {
       EntityManagerFactory factory = Persistence.createEntityManagerFactory("Books");
       EntityManager manager = factory.createEntityManager();
       // Create an instance of CarClub and store our new EntityManager as an instance variable.
+=======
+public class BookJPA {
+    /**
+     * You will likely need the entityManager in a great many functions throughout your application.
+     * Rather than make this a global variable, we will make it an instance variable within the CarClub
+     * class, and create an instance of CarClub in the main.
+     */
+    private EntityManager entityManager;
+
+    /**
+     * The Logger can easily be configured to log to a file, rather than, or in addition to, the console.
+     * We use it because it is easy to control how much or how little logging gets done without having to
+     * go through the application and comment out/uncomment code and run the risk of introducing a bug.
+     * Here also, we want to make sure that the one Logger instance is readily available throughout the
+     * application, without resorting to creating a global variable.
+     */
+    private static final Logger LOGGER = Logger.getLogger(BookJPA.class.getName());
+
+    /**
+     * The constructor for the CarClub class.  All that it does is stash the provided EntityManager
+     * for use later in the application.
+     * @param manager    The EntityManager that we will use.
+     */
+    public BookJPA(EntityManager manager) {
+        this.entityManager = manager;
+    }
+
+    public static void main(String[] args) {
+        LOGGER.fine("Creating EntityManagerFactory and EntityManager");
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("BookJPA");
+        EntityManager manager = factory.createEntityManager();
+        // Create an instance of Book and store our new EntityManager as an instance variable.
+        BookJPA books = new BookJPA(manager);
+
+        LOGGER.fine("Begin of Transaction");
+        EntityTransaction tx = manager.getTransaction();
+        tx.begin();
+
+        List<AuthoringEntities> authorings = new ArrayList<>();
+        authorings.add(new AuthoringEntities("Anne Frank", "annefrank@gmail.com", "Individual Author"));
+        authorings.add(new AuthoringEntities("Susan Cain", "susancain@gmail.com", "Individual Author"));
+        books.createEntity(authorings);
+        System.out.println("1. Add new objects");
+        System.out.println("2. List all the information about a specific Object:");
+        System.out.println("3. Delete a book");
+        System.out.println("4. Update a book");
+        System.out.println("5. List the the primary key of all rows of: ");
+
+        Scanner input = new Scanner(System.in);
+        int choice = Integer.parseInt(input.next());
+        int subChoice = 0;
+        switch (choice) {
+            case 1: {
+                tx.begin();
+                String name;
+                String email;
+
+                System.out.println("1. Add a Writing Group");
+                System.out.println("2. Add an Individual Author");
+                System.out.println("3. Add an Ad Hoc Team");
+                subChoice = Integer.parseInt(input.next());
+                switch (subChoice) {
+                    case 1:
+                        System.out.println("Enter authoring entity name:");
+                        name = getUserString();
+                        System.out.println("Enter authoring entity email:");
+                        email = getUserString();
+
+
+
+
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                }
+                break;
+            }
+
+            case 2: {
+                System.out.println("1. Add a Writing Group");
+                System.out.println("2. Add an Individual Author");
+                System.out.println("3. Add an Ad Hoc Team");
+                subChoice = Integer.parseInt(input.next());
+                switch (subChoice) {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+
+                }
+                break;
+            }
+
+            case 3: {
+                System.out.println("Please enter the candidate keys in order to delete a book ");
+                // check for candidate keys
+
+
+            }
+            break;
+
+            case 4: {
+                System.out.println("Change the book's authoring entities: ");
+            }
+            break;
+
+            case 5: {
+                System.out.println("Show primary key for Publishers");
+                System.out.println("Show primary key for Books");
+                System.out.println("Show primary key for Authoring Entities");
+                subChoice = Integer.parseInt(input.next());
+                switch (subChoice) {
+                    case 1:
+
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                }
+                break;
+            }
+        }
+
+        List<IndividualAuthors> idvAuthors = new ArrayList<>();
+        books.createEntity(idvAuthors);
+
+        List<WritingGroups> writingGrp = new ArrayList<>();
+        books.createEntity(writingGrp);
+
+        List<AdHocTeams> adHocTeams = new ArrayList<>();
+        books.createEntity(adHocTeams);
+
+        List<AdHocTeamMembers> adHocMems = new ArrayList<>();
+        books.createEntity(adHocMems);
+
+        List<Publishers> publishers = new ArrayList<>();
+        publishers.add(new Publishers("Penguine House Publisher", "714-666-777", "penguinepublisher@gmail.com"));
+        books.createEntity((publishers));
+
+        //books.prompts();
+        tx.commit();
+
+        // Create an instance of CarClub and store our new EntityManager as an instance variable.
+>>>>>>> Stashed changes:src/main/java/csulb/cecs323/app/BookJPA.java
 //      Books carclub = new Books(manager);
 //
 //
@@ -85,6 +235,7 @@ public class Books {
 //
 //      // Commit the changes so that the new data persists and is visible to other users.
 //      tx.commit();
+<<<<<<< Updated upstream:src/main/java/csulb/cecs323/app/Books.java
       LOGGER.fine("End of Transaction");
 
    } // End of the main method
@@ -109,4 +260,40 @@ public class Books {
          LOGGER.info("Persisted object after flush (non-null id): " + next);
       }
    } // End of createEntity member method
+=======
+        LOGGER.fine("End of Transaction");
+
+    } // End of the main method
+
+    /**
+     * Gathers a string from the user.
+     * @return the string the user put in
+     */
+    public static String getUserString() {
+        Scanner in = new Scanner(System.in);
+        return in.nextLine();
+    }
+
+
+    /**
+     * Create and persist a list of objects to the database.
+     * @param entities   The list of entities to persist.  These can be any object that has been
+     *                   properly annotated in JPA and marked as "persistable."  I specifically
+     *                   used a Java generic so that I did not have to write this over and over.
+     */
+    public <E> void createEntity(List <E> entities) {
+        for (E next : entities) {
+            LOGGER.info("Persisting: " + next);
+            // Use the CarClub entityManager instance variable to get our EntityManager.
+            this.entityManager.persist(next);
+        }
+
+        // The auto generated ID (if present) is not passed in to the constructor since JPA will
+        // generate a value.  So the previous for loop will not show a value for the ID.  But
+        // now that the Entity has been persisted, JPA has generated the ID and filled that in.
+        for (E next : entities) {
+            LOGGER.info("Persisted object after flush (non-null id): " + next);
+        }
+    } // End of createEntity member method
+>>>>>>> Stashed changes:src/main/java/csulb/cecs323/app/BookJPA.java
 }// End of the getStyle method
